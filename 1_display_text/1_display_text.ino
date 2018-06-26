1a21b70a617b4b6f25b26cf70a6e5a2573a6784c
Muhammad Irfan Luthfi <iir_irfan02@hotmail.com>
2018-06-26 10:11:38 +0700

    Add files via upload

---
+++ 1_display_text/1_display_text.ino
@@ -0,0 +1,30 @@
+#include <UTFT.h> 
+#include <URTouch.h>
+
+UTFT    myGLCD(SSD1289,38,39,40,41);
+
+extern uint8_t BigFont[];
+extern uint8_t SmallFont[];
+
+void setup() {
+  myGLCD.InitLCD();
+  myGLCD.clrScr();
+  drawScreen();
+}
+
+void loop() {
+  drawScreen();
+}
+
+void drawScreen(){
+//Title
+  myGLCD.setColor(255, 255, 255); // Set font color
+  myGLCD.setFont(BigFont); // Set font size
+  myGLCD.print("HELLO WORLD!", LEFT, 10); //Print String
+
+//SubTitle
+  myGLCD.setColor(255, 255, 255); // Set font color
+  myGLCD.setFont(SmallFont); // Set font size
+  myGLCD.print("by: milstrike", LEFT, 41); // Print String
+}
+
