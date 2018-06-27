8137b73871cefc15dc0b9bee37fc1e2d0bab4341
Muhammad Irfan Luthfi <iir_irfan02@hotmail.com>
2018-06-27 20:03:58 +0700

    Add files via upload

---
+++ 7_portrait_view/7_portrait_view.ino
@@ -0,0 +1,31 @@
+//Code by milstrike
+#include <UTFT.h> 
+#include <URTouch.h>
+
+UTFT    myGLCD(SSD1289,38,39,40,41);
+
+extern uint8_t BigFont[];
+extern uint8_t SmallFont[];
+
+void setup() {
+  myGLCD.InitLCD(0); //0: Portrait 1: Landscape
+  myGLCD.clrScr();
+  drawScreen();
+}
+
+void loop() {
+  drawScreen();
+}
+
+void drawScreen(){
+//Title
+  myGLCD.setColor(255, 255, 255); // Set font color
+  myGLCD.setFont(BigFont); // Set font size
+  myGLCD.print("HELLO WORLD!", LEFT, 10); //Print String
+
+//SubTitle
+  myGLCD.setColor(255, 255, 255); // Set font color
+  myGLCD.setFont(SmallFont); // Set font size
+  myGLCD.print("by: milstrike", LEFT, 41); // Print String
+}
+
