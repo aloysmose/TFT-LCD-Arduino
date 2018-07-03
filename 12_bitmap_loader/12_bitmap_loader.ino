29121e2afadec7c09c5141f9c0432727358f43f0
Muhammad Irfan Luthfi <iir_irfan02@hotmail.com>
2018-07-03 21:09:28 +0700

    Add files via upload

---
+++ 12_bitmap_loader/12_bitmap_loader.ino
@@ -0,0 +1,20 @@
+#include <UTFT.h> 
+
+UTFT myGLCD(SSD1289,38,39,40,41);
+
+extern uint8_t BigFont[];
+extern unsigned int milsystem[0x2A8A];
+
+void setup() {
+  myGLCD.InitLCD(1); //0: Portrait 1: Landscape
+  myGLCD.clrScr();
+  drawScreen();
+}
+
+void loop() {
+}
+
+void drawScreen(){
+  myGLCD.drawBitmap (38,97, 242, 45, milsystem);
+}
+
